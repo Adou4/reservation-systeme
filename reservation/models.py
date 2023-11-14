463564295f87a0a037728d5cfb5cc99570de7bcb
Adou4 <bocouma326@gmail.com>
2023-11-14 12:51:48 +0000

    commit

---
+++ reservation/models.py
@@ -0,0 +1,13 @@
+# reservation/models.py
+from django.db import models
+from django.contrib.auth.models import User
+
+class Location(models.Model):
+    name = models.CharField(max_length=255)
+
+class Reservation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    date = models.DateField()
+    time_start = models.TimeField()
+    time_end = models.TimeField()
